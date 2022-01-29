810c10256d6adea739e27d7017c011988deae478
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-01-29 09:30:37 +0900

    ルート、ルートサービスプロバイダの設定

--- routes/companies.php
+++ routes/companies.php
@@ -1,14 +1,33 @@
 <?php
 
-use App\Http\Controllers\User\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\User\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\User\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\User\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\User\Auth\NewPasswordController;
-use App\Http\Controllers\User\Auth\PasswordResetLinkController;
-use App\Http\Controllers\User\Auth\RegisteredUserController;
-use App\Http\Controllers\User\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Companies\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Companies\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Companies\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Companies\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Companies\Auth\NewPasswordController;
+use App\Http\Controllers\Companies\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Companies\Auth\RegisteredUserController;
+use App\Http\Controllers\Companies\Auth\VerifyEmailController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
   ->middleware('guest')
