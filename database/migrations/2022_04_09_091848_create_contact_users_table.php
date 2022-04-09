680e064877ad502526e9e65e8a107c3b29547fa4
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-04-09 09:22:35 +0900

    contactUsersテーブル、モデルの作成

---
+++ database/migrations/2022_04_09_091848_create_contact_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('companies_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->boolean('follow')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_users');
+    }
+}
