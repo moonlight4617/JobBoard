7e99d90da12730b0d3e599b5dd9cf2131391a65d
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-05-05 16:56:29 +0900

    occupationSeederの作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             companySeeder::class,
-            JobSeeder::class
+            JobSeeder::class,
+            OccupationSeeder::class
         ]);
     }
 }
