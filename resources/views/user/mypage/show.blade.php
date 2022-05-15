436967a3a675d6c41e0447276505b9912dd1c607
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-05-15 16:53:43 +0900

    user プロフィール画面修正

--- resources/views/user/mypage/show.blade.php
+++ resources/views/user/mypage/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            ユーザー情報
+            マイプロフィール
         </h2>
     </x-slot>
 
@@ -12,88 +12,110 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font relative">
                         <div class="container px-5 py-4 mx-auto">
-                            <div class="flex flex-col text-center w-full mb-12">
-                                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">ユーザー情報</h1>
-                            </div>
                             <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                            <div class="lg md:w-2/3 mx-auto">
+                            <div class="md:w-3/5 mx-auto mt-4">
+                                <div class="flex items-center pb-10 border-gray-200 sm:flex-row flex-col">
+                                    <div class="sm:mr-10 inline-flex items-center justify-center">
+                                        @if (empty($user->pro_image))
+                                            <img class="rounded-full h-40 w-40"
+                                                src="https://via.placeholder.com/100x100?text=No+Image">
+                                        @else
+                                            <img class="rounded-full h-40 w-40 object-cover"
+                                                src="{{ asset('storage/users/' . $user->pro_image) }}">
+                                        @endif
+                                    </div>
+                                    <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
+                                        <div class="flex items-center mb-2">
+                                            <p class="text-gray-900 title-font font-bold text-4xl">
+                                                {{ $user->name }}
+                                            </p>
+                                        </div>
+                                        <p class="leading-relaxed text-xl">{{ $user->catch }}</p>
+                                    </div>
+                                </div>
 
-                                @if (empty($user->pro_image))
-                                    <img src="https://via.placeholder.com/1980x1080?text=No+Image">
-                                @else
-                                    <img src="{{ asset('storage/users/' . $user->pro_image) }}">
-                                @endif
-
                                 <div class="p-2">
                                     <div class="relative">
-                                        ユーザー名：{{ $user->name }}
+                                        <div class="my-4">
+                                            <span class="font-bold border p-1 border-gray-400 rounded">自己紹介文</span>
+                                        </div>
+                                        <p class="ml-2">{{ $user->intro }}</p>
                                     </div>
                                 </div>
                                 <div class="p-2">
                                     <div class="relative">
-                                        キャッチコピー：{{ $user->catch }}
+                                        <div class="my-4">
+                                            <span class="font-bold border p-1 border-gray-400 rounded">経歴</span>
+                                        </div>
+                                        <p class="ml-2">{{ $user->career }}</p>
                                     </div>
                                 </div>
                                 <div class="p-2">
                                     <div class="relative">
-                                        紹介文：{{ $user->intro }}
+                                        <div class="my-4">
+                                            <span class="font-bold border p-1 border-gray-400 rounded">資格</span>
+                                        </div>
+                                        <p class="ml-2">{{ $user->license }}</p>
                                     </div>
                                 </div>
                                 <div class="p-2">
                                     <div class="relative">
-                                        Eメール：{{ $user->email }}
-                                    </div>
-                                </div>
-                                <div class="p-2">
-                                    <div class="relative">
-                                        資格：{{ $user->license }}
-                                    </div>
-                                </div>
-                                <div class="p-2">
-                                    <div class="relative">
-                                        経歴：{{ $user->career }}
-                                    </div>
-                                </div>
-                                <div class="p-2">
-                                    <div class="relative">
-                                        趣味・特技：{{ $user->hobby }}
+                                        <div class="my-4">
+                                            <span class="font-bold border p-1 border-gray-400 rounded">趣味・特技</span>
+                                        </div>
+                                        <p class="ml-2">{{ $user->hobby }}</p>
                                     </div>
                                 </div>
 
                                 @if ($tags)
                                     <div class="p-2">
-                                        <p>特徴タグ</p>
+                                        <div class="my-4">
+                                            <span class="font-bold border p-1 border-gray-400 rounded">特徴タグ</span>
+                                        </div>
                                         @foreach ($tags as $tag)
                                             <div class="relative inline-block px-1 py-2">
-                                                <label for="checkbox1"
-                                                    class="text-white rounded-full bg-teal-500  cursor-pointer ease-in px-2 py-1">{{ $tag->tag_name }}
-                                                </label>
+                                                <div class="text-white rounded-full bg-teal-500 ease-in px-2 py-1">
+                                                    {{ $tag->tag_name }}
+                                                </div>
                                             </div>
                                         @endforeach
                                     </div>
                                 @endif
 
                                 <div class="p-2">
-                                    <div class="relative flex">
-                                        <p>SNS：</p>
+                                    <div class="my-4">
+                                        <span class="font-bold border p-1 border-gray-400 rounded">SNS</span>
+                                    </div>
+                                    <div class="relative flex ml-2">
                                         @if ($user->twitter)
-                                            <img src="/images/twitter-brands.svg" class="w-8" />
+                                            <a href="https://{{ $user->twitter }}" target="_blank">
+                                                <img src="/images/twitter-brands.svg" class="w-8 mx-2" />
+                                            </a>
                                         @endif
                                         @if ($user->youtube)
-                                            <img src="/images/youtube-brands.svg" class="w-8" />
+                                            <a href="https://{{ $user->youtube }}" target="_blank">
+                                                <img src="/images/youtube-brands.svg" class="w-8 mx-2" />
+                                            </a>
                                         @endif
                                         @if ($user->insta)
-                                            <img src="/images/instagram-brands.svg" class="w-8" />
+                                            <a href="https://{{ $user->insta }}" target="_blank">
+                                                <img src="/images/instagram-brands.svg" class="w-8 mx-2" />
+                                            </a>
                                         @endif
                                         @if ($user->blog)
-                                            <img src="/images/blogger-brands.svg" class="w-8" alt="ブログ" />
+                                            <a href="https://{{ $user->blog }}" target="_blank">
+                                                <img src="/images/blogger-brands.svg" class="w-8 mx-2"
+                                                    alt="ブログ" />
+                                            </a>
                                         @endif
                                     </div>
                                 </div>
 
                                 <div class="p-2">
                                     <div class="relative">
-                                        <p>ポートフォリオ</p>
+                                        <div class="my-4">
+                                            <span class="font-bold border p-1 border-gray-400 rounded">ポートフォリオ</span>
+                                        </div>
                                     </div>
                                 </div>
                                 {{-- ユーザーアップロード画像 --}}
