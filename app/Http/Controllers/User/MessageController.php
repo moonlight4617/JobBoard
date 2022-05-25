f1ae1608c26624bccc057c1f81d29e4d04566e5e
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-05-25 09:24:24 +0900

    user側メッセージ一覧画面修正

--- app/Http/Controllers/User/MessageController.php
+++ app/Http/Controllers/User/MessageController.php
@@ -8,14 +8,23 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Message;
 use App\Models\ContactUsers;
 use App\Models\Companies;
+use App\Models\User;
+use App\Models\Jobs;
+use App\Models\AppStatus;
 use Carbon\Carbon;
 
 class MessageController extends Controller
 {
     public function index()
     {
-        $companies = ContactUsers::where('users_id', Auth::id())->with('companies')->get();
-        return view('user.message.index', compact(['companies']));
+        $companies = ContactUsers::where('users_id', Auth::id())->with('companies')->with('messages')->get();
+
+        // $users = ContactUsers::where('companies_id', Auth::id())->with('users')->with('messages')->get();
+        // $jobs = User::findOrFail(Auth::id())->appStatus()->where('app_flag', 1)->jobs()->get();
+        $jobsId = AppStatus::where('users_id', Auth::id())->where('app_flag', 1)->pluck('jobs_id');
+        $jobs = Jobs::whereIn('id', $jobsId)->get();
+
+        return view('user.message.index', compact(['companies', 'jobs']));
     }
 
     public function show($id)
