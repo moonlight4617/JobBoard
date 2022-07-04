d2376553f09c3b54ea07b7ead95af8a1182e9f63
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-07-05 08:09:15 +0900

    トップページがログイン済みかどうかで表示切り替え

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -4,13 +4,18 @@
     </x-slot>
 
     <div class="relative flex items-top justify-center bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        <div class="fixed top-0 right-0 px-6 py-4 block">
-            @if (Route::has('company.login'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('company.login') }}">
-                    {{ __('企業はこちら') }}
-                </a>
-            @endif
-        </div>
+
+        @auth('users')
+        @else
+            <div class="fixed top-0 right-0 px-6 py-4 block">
+                @if (Route::has('company.login'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('company.login') }}">
+                        {{ __('企業はこちら') }}
+                    </a>
+                @endif
+            </div>
+        @endauth
+
         <div
             class="container bg-no-repeat sm:w-4/5 md:w-4/5 pt-12 sm:pt-24 sm:mt-12 pb-12 bg-[url('/images/topImage-white.png')] bg-contain bg-center">
             <div class="text-center"><small>飲食の求人サイト</small></div>
@@ -21,12 +26,17 @@
             </p>
             <br />
             <div class="flex justify-around mt-4 px-4">
-                <button type="button"
-                    class="bg-white border-0 py-2 px-8 focus:outline-none hover:outline-none rounded text-lg"
-                    onclick="location.href='{{ route('user.register') }}'">登録</button>
-                <button type="button"
-                    class="bg-blue-300 border-0 py-2 px-8 focus:outline-none hover:bg-blue-400 rounded text-lg"
-                    onclick="location.href='{{ route('user.login') }}'">ログイン</button>
+
+                @auth('users')
+                @else
+                    <button type="button"
+                        class="bg-white border-0 py-2 px-8 focus:outline-none hover:outline-none rounded text-lg"
+                        onclick="location.href='{{ route('user.register') }}'">登録</button>
+                    <button type="button"
+                        class="bg-blue-300 border-0 py-2 px-8 focus:outline-none hover:bg-blue-400 rounded text-lg"
+                        onclick="location.href='{{ route('user.login') }}'">ログイン</button>
+                @endauth
+
             </div>
         </div>
     </div>
