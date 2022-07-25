cd50b389eff065f24c87970f72eb9cd44483f5cd
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-07-25 22:39:34 +0900

    seeder修正。外部キーの指定の仕方変更

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Companies;
 
 class JobSeeder extends Seeder
 {
@@ -14,9 +15,12 @@ class JobSeeder extends Seeder
      */
     public function run()
     {
+        $conpany1Id = Companies::where('name', 'Company1')->value('id');
+        $conpany2Id = Companies::where('name', 'Company2')->value('id');
+        $conpany3Id = Companies::where('name', 'Company3')->value('id');
         DB::table('jobs')->insert([
             [
-                'companies_id' => 1,
+                'companies_id' => $conpany1Id,
                 'job_name' => 'イタリアンのキッチン',
                 'detail' => '赤坂にあるオシャレなイタリアンレストランでのキッチン業務になります。とかなんとか',
                 'conditions' => '未経験可',
@@ -29,7 +33,7 @@ class JobSeeder extends Seeder
                 'catch' => 'オープニングメンバーとして働きませんか？',
             ],
             [
-                'companies_id' => 2,
+                'companies_id' => $conpany2Id,
                 'job_name' => '老舗割烹料理屋のホール',
                 'detail' => '創設50年を越える歴史ある料理屋です。とかなんとか',
                 'conditions' => '経験優遇',
@@ -42,7 +46,7 @@ class JobSeeder extends Seeder
                 'catch' => '歴史ある老舗でサービスを身につけませんか？',
             ],
             [
-                'companies_id' => 3,
+                'companies_id' => $conpany3Id,
                 'job_name' => 'パティシエ',
                 'detail' => 'パティシエ誰々監修の洋菓子店でパティシエ募集します。とかなんとか',
                 'conditions' => '特になし',
@@ -55,5 +59,46 @@ class JobSeeder extends Seeder
                 'catch' => '技術を身につけるチャンス！',
             ],
         ]);
+        // DB::table('jobs')->insert([
+        //     [
+        //         'companies_id' => 1,
+        //         'job_name' => 'イタリアンのキッチン',
+        //         'detail' => '赤坂にあるオシャレなイタリアンレストランでのキッチン業務になります。とかなんとか',
+        //         'conditions' => '未経験可',
+        //         'duty_hours' => '9:00~21:00',
+        //         'low_salary' => '260',
+        //         'high_salary' => '600',
+        //         'holiday' => 'シフト制。月６回',
+        //         'benefits' => '社会保険完備',
+        //         'emp_status' => '4',
+        //         'catch' => 'オープニングメンバーとして働きませんか？',
+        //     ],
+        //     [
+        //         'companies_id' => 2,
+        //         'job_name' => '老舗割烹料理屋のホール',
+        //         'detail' => '創設50年を越える歴史ある料理屋です。とかなんとか',
+        //         'conditions' => '経験優遇',
+        //         'duty_hours' => '11:00~23:00',
+        //         'low_salary' => '300',
+        //         'high_salary' => '480',
+        //         'holiday' => 'シフト制。月8回',
+        //         'benefits' => '社会保険完備',
+        //         'emp_status' => '0',
+        //         'catch' => '歴史ある老舗でサービスを身につけませんか？',
+        //     ],
+        //     [
+        //         'companies_id' => 3,
+        //         'job_name' => 'パティシエ',
+        //         'detail' => 'パティシエ誰々監修の洋菓子店でパティシエ募集します。とかなんとか',
+        //         'conditions' => '特になし',
+        //         'duty_hours' => '8:00~19:00',
+        //         'low_salary' => '240',
+        //         'high_salary' => '360',
+        //         'holiday' => 'シフト制。月8回',
+        //         'benefits' => '社会保険完備',
+        //         'emp_status' => '2',
+        //         'catch' => '技術を身につけるチャンス！',
+        //     ],
+        // ]);
     }
 }
