ae28f672d7b737ee1c62242b3d2251bef7ba7ac4
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-07-27 08:07:16 +0900

    companies routeのverifiedを一旦無効化

--- routes/companies.php
+++ routes/companies.php
@@ -88,35 +88,56 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
   ->name('logout');
 
 // 求人
-Route::get('/jobs', [JobsController::class, 'index'])->middleware(['auth:companies', 'verified'])->name('jobs.index');
-Route::get('/jobs/previous', [JobsController::class, 'previousIndex'])->middleware(['auth:companies', 'verified'])->name('jobs.previous');
-Route::get('/jobs/create', [JobsController::class, 'create'])->middleware(['auth:companies', 'verified'])->name('jobs.create');
-Route::post('/jobs', [JobsController::class, 'store'])->middleware(['auth:companies', 'verified'])->name('jobs.store');
-Route::get('/jobs/{job}', [JobsController::class, 'show'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.show');
-Route::get('/jobs/{job}/edit', [JobsController::class, 'edit'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.edit');
-Route::post('/jobs/{job}/close', [JobsController::class, 'close'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.close');
-Route::post('/jobs/{job}/resume', [JobsController::class, 'resume'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.resume');
-Route::get('/jobs/{job}/appliedIndex', [JobsController::class, 'appliedIndex'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.appliedIndex');
-Route::put('/jobs/{job}', [JobsController::class, 'update'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.update');
-Route::delete('/jobs/{job}', [JobsController::class, 'destroy'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.destroy');
+Route::get('/jobs', [JobsController::class, 'index'])->middleware(['auth:companies'])->name('jobs.index');
+// Route::get('/jobs', [JobsController::class, 'index'])->middleware(['auth:companies', 'verified'])->name('jobs.index');
+Route::get('/jobs/previous', [JobsController::class, 'previousIndex'])->middleware(['auth:companies'])->name('jobs.previous');
+// Route::get('/jobs/previous', [JobsController::class, 'previousIndex'])->middleware(['auth:companies', 'verified'])->name('jobs.previous');
+Route::get('/jobs/create', [JobsController::class, 'create'])->middleware(['auth:companies'])->name('jobs.create');
+// Route::get('/jobs/create', [JobsController::class, 'create'])->middleware(['auth:companies', 'verified'])->name('jobs.create');
+Route::post('/jobs', [JobsController::class, 'store'])->middleware(['auth:companies'])->name('jobs.store');
+// Route::post('/jobs', [JobsController::class, 'store'])->middleware(['auth:companies', 'verified'])->name('jobs.store');
+Route::get('/jobs/{job}', [JobsController::class, 'show'])->middleware(['ensure.jobCompany', 'auth:companies'])->name('jobs.show');
+// Route::get('/jobs/{job}', [JobsController::class, 'show'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.show');
+Route::get('/jobs/{job}/edit', [JobsController::class, 'edit'])->middleware(['ensure.jobCompany', 'auth:companies'])->name('jobs.edit');
+// Route::get('/jobs/{job}/edit', [JobsController::class, 'edit'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.edit');
+Route::post('/jobs/{job}/close', [JobsController::class, 'close'])->middleware(['ensure.jobCompany', 'auth:companies'])->name('jobs.close');
+// Route::post('/jobs/{job}/close', [JobsController::class, 'close'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.close');
+Route::post('/jobs/{job}/resume', [JobsController::class, 'resume'])->middleware(['ensure.jobCompany', 'auth:companies'])->name('jobs.resume');
+// Route::post('/jobs/{job}/resume', [JobsController::class, 'resume'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.resume');
+Route::get('/jobs/{job}/appliedIndex', [JobsController::class, 'appliedIndex'])->middleware(['ensure.jobCompany', 'auth:companies'])->name('jobs.appliedIndex');
+// Route::get('/jobs/{job}/appliedIndex', [JobsController::class, 'appliedIndex'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.appliedIndex');
+Route::put('/jobs/{job}', [JobsController::class, 'update'])->middleware(['ensure.jobCompany', 'auth:companies'])->name('jobs.update');
+// Route::put('/jobs/{job}', [JobsController::class, 'update'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.update');
+Route::delete('/jobs/{job}', [JobsController::class, 'destroy'])->middleware(['ensure.jobCompany', 'auth:companies'])->name('jobs.destroy');
+// Route::delete('/jobs/{job}', [JobsController::class, 'destroy'])->middleware(['ensure.jobCompany', 'auth:companies', 'verified'])->name('jobs.destroy');
 // Route::resource('jobs', JobsController::class)->middleware('auth:companies');
 
-Route::resource('company', CompanyController::class, ['except' => 'index'])->middleware(['ensure.company', 'auth:companies', 'verified']);
+Route::resource('company', CompanyController::class, ['except' => 'index'])->middleware(['ensure.company', 'auth:companies']);
+// Route::resource('company', CompanyController::class, ['except' => 'index'])->middleware(['ensure.company', 'auth:companies', 'verified']);
 
 // 人材
-Route::get('/hresource', [JobSeeker::class, 'index'])->middleware(['auth:companies', 'verified'])->name('user.index');
-Route::get('/hresource/followIndex', [JobSeeker::class, 'followIndex'])->middleware(['auth:companies', 'verified'])->name('user.followIndex');
-Route::get('/hresource/search', [JobSeeker::class, 'search'])->middleware(['auth:companies', 'verified'])->name('user.index.search');
-Route::get('/hresource/followSearch', [JobSeeker::class, 'followSearch'])->middleware(['auth:companies', 'verified'])->name('user.followIndex.search');
-Route::get('/hresource/{user}', [JobSeeker::class, 'show'])->middleware(['auth:companies', 'verified'])->name('user.show');
+Route::get('/hresource', [JobSeeker::class, 'index'])->middleware(['auth:companies'])->name('user.index');
+// Route::get('/hresource', [JobSeeker::class, 'index'])->middleware(['auth:companies', 'verified'])->name('user.index');
+Route::get('/hresource/followIndex', [JobSeeker::class, 'followIndex'])->middleware(['auth:companies'])->name('user.followIndex');
+// Route::get('/hresource/followIndex', [JobSeeker::class, 'followIndex'])->middleware(['auth:companies', 'verified'])->name('user.followIndex');
+Route::get('/hresource/search', [JobSeeker::class, 'search'])->middleware(['auth:companies'])->name('user.index.search');
+// Route::get('/hresource/search', [JobSeeker::class, 'search'])->middleware(['auth:companies', 'verified'])->name('user.index.search');
+Route::get('/hresource/followSearch', [JobSeeker::class, 'followSearch'])->middleware(['auth:companies'])->name('user.followIndex.search');
+// Route::get('/hresource/followSearch', [JobSeeker::class, 'followSearch'])->middleware(['auth:companies', 'verified'])->name('user.followIndex.search');
+Route::get('/hresource/{user}', [JobSeeker::class, 'show'])->middleware(['auth:companies'])->name('user.show');
+// Route::get('/hresource/{user}', [JobSeeker::class, 'show'])->middleware(['auth:companies', 'verified'])->name('user.show');
 
 // フォロー機能
-Route::post('/hresource/follow', [JobSeeker::class, 'follow'])->middleware(['auth:companies', 'verified'])->name('user.follow');
+Route::post('/hresource/follow', [JobSeeker::class, 'follow'])->middleware(['auth:companies'])->name('user.follow');
+// Route::post('/hresource/follow', [JobSeeker::class, 'follow'])->middleware(['auth:companies', 'verified'])->name('user.follow');
 
 // メッセージ
-Route::get('/messages', [MessageController::class, 'index'])->middleware(['auth:companies', 'verified'])->name('message.index');
-Route::post('/messages/{user}/post', [MessageController::class, 'post'])->middleware(['auth:companies', 'verified'])->name('message.post');
-Route::get('/messages/{user}', [MessageController::class, 'show'])->middleware(['auth:companies', 'verified'])->name('message.show');
+Route::get('/messages', [MessageController::class, 'index'])->middleware(['auth:companies'])->name('message.index');
+// Route::get('/messages', [MessageController::class, 'index'])->middleware(['auth:companies', 'verified'])->name('message.index');
+Route::post('/messages/{user}/post', [MessageController::class, 'post'])->middleware(['auth:companies'])->name('message.post');
+// Route::post('/messages/{user}/post', [MessageController::class, 'post'])->middleware(['auth:companies', 'verified'])->name('message.post');
+Route::get('/messages/{user}', [MessageController::class, 'show'])->middleware(['auth:companies'])->name('message.show');
+// Route::get('/messages/{user}', [MessageController::class, 'show'])->middleware(['auth:companies', 'verified'])->name('message.show');
 
 
 // ユーザーとしてログイン
