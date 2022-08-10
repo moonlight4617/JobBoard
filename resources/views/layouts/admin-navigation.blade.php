505bf2aa3a68b66feee0926b53cb1d90f0ac5741
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2022-08-10 14:36:35 +0900

    管理者がタグを新規作成、削除できるよう実装

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -5,16 +5,14 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('admin.dashboard') }}">
+                    {{-- <a href="{{ route('admin.dashboard') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
+                    </a> --}}
+                    <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    {{-- <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link> --}}
                     <x-nav-link :href="route('admin.companies.index')" :active="request()->routeIs('admin.companies.index')">
                         企業一覧
                     </x-nav-link>
@@ -26,6 +24,9 @@
                     </x-nav-link>
                     <x-nav-link :href="route('admin.users.create')" :active="request()->routeIs('admin.users.create')">
                         ユーザー登録
+                    </x-nav-link>
+                    <x-nav-link :href="route('admin.tags.index')" :active="request()->routeIs('admin.tags.index')">
+                        タグ編集
                     </x-nav-link>
                 </div>
             </div>
@@ -54,7 +55,8 @@
                         <form method="POST" action="{{ route('admin.logout') }}">
                             @csrf
 
-                            <x-dropdown-link :href="route('admin.logout')" onclick="event.preventDefault();
+                            <x-dropdown-link :href="route('admin.logout')"
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 ログアウト
                             </x-dropdown-link>
@@ -68,10 +70,11 @@
                 <button @click="open = ! open"
                     class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
@@ -79,12 +82,12 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
+        {{-- <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-        </div>
+        </div> --}}
         <div class="pt-2 pb-3 pl-3 space-y-1">
             <x-nav-link :href="route('admin.companies.index')" :active="request()->routeIs('admin.companies.index')">
                 企業一覧
@@ -93,6 +96,21 @@
         <div class="pt-2 pb-3 pl-3 space-y-1">
             <x-nav-link :href="route('admin.companies.create')" :active="request()->routeIs('admin.companies.create')">
                 企業登録
+            </x-nav-link>
+        </div>
+        <div class="pt-2 pb-3 pl-3 space-y-1">
+            <x-nav-link :href="route('admin.users.index')" :active="request()->routeIs('admin.users.index')">
+                ユーザー一覧
+            </x-nav-link>
+        </div>
+        <div class="pt-2 pb-3 pl-3 space-y-1">
+            <x-nav-link :href="route('admin.users.create')" :active="request()->routeIs('admin.users.create')">
+                ユーザー登録
+            </x-nav-link>
+        </div>
+        <div class="pt-2 pb-3 pl-3 space-y-1">
+            <x-nav-link :href="route('admin.tags.index')" :active="request()->routeIs('admin.tags.index')">
+                タグ編集
             </x-nav-link>
         </div>
 
@@ -108,7 +126,8 @@
                 <form method="POST" action="{{ route('admin.logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('admin.logout')" onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('admin.logout')"
+                        onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         ログアウト
                     </x-responsive-nav-link>
